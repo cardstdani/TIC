9c1900fddba19dfb8ca06fdf28de7d225a8c1743
cardstdani <72665778+cardstdani@users.noreply.github.com>
2022-03-03 19:47:27 +0100

    Update Ejercicio1.py

--- Python/Ejercicio1.py
+++ Python/Ejercicio1.py
@@ -1,28 +1,52 @@
 l = []
-for i in range(3):
+i = 0
+while i < 3:
     inp = input("Introduce un número:\n")
     if inp.isnumeric():
         l.append(inp)
+        i += 1
     else:
         print("Error;\n")
 
-def merge(li1, li2):
+def merge(l1, l2):
     rl = []
     
-    i, j = 0, 0
-    while i < len(li1) and j < len(li2):
-        if li1[i] <= li2[j]:
-            rl.append(li1[i])
-            i += 1
-        else:
-            rl.append(li2[j])
-            j += 1
+    while l1 != [] or l2 != []:
+        try:
+            if l1[0] <= l2[0]:
+                rl.append(l1[0])
+                l1.pop(0)
+            else:
+                rl.append(l2[0])
+                l2.pop(0)
+        except:
+            if l1 == []:
+                m = min(l2)
+                rl.append(m)
+                l2.pop(l2.index(m))
+            else:
+                m = min(l1)
+                rl.append(m)
+                l1.pop(l1.index(m))
     return rl
-            
     
 def mergeSort(li):
     if len(li) <= 1:
         return li
-    return merge(mergeSort(li[:len(li)//2]), mergeSort(li[len(li)//2:]))
+    mid = len(li)//2
+    return merge(mergeSort(li[:mid]), mergeSort(li[mid:]))
 
 print(",".join(mergeSort(l)))
+
+#Solución simple
+l = []
+i = 0
+while i < 3:
+    inp = input("Introduce un número:\n")
+    if inp.isnumeric():
+        l.append(inp)
+        i += 1
+    else:
+        print("Error;\n")
+
+print(",".join(sorted(l)))
