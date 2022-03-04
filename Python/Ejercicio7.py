bf381df4565084bd6abee254c33d350fd65fac78
cardstdani <72665778+cardstdani@users.noreply.github.com>
2022-03-04 18:04:23 +0100

    Update Ejercicio7.py

--- Python/Ejercicio7.py
+++ Python/Ejercicio7.py
@@ -1,4 +1,12 @@
 def inverso(n):
     return int(str(n)[::-1])
 
-print(inverso(3456))
+def capicua(n):
+    return n == inverso(n)
+
+while True:
+    inp = int(input("Introduce un número:\n"))
+    
+    if inp < 0:
+        break
+    print(f"El número {inp} es capicua" if capicua(inp) else f"El número {inp} es capicua")
