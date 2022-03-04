305867020b2b120d7773d098bce305c0dcf3c63a
cardstdani <72665778+cardstdani@users.noreply.github.com>
2022-03-04 10:25:38 +0100

    Update Ejercicio5.py

--- Python/Ejercicio5.py
+++ Python/Ejercicio5.py
@@ -8,8 +8,10 @@ l.append(input("NIF del cliente: \n"))
 l.append(input("Dirección del cliente: \n"))
 l.append(input("Nombre del producto: \n"))
 
-total = l[0]*l[1]
-descuento = -((total)-(total)*0.85) * (1 if l[2] == "s" else 0)
+total = round(l[0]*l[1], 2)
+descuento = round(-((total)*0.15) * (1 if l[2] == "s" else 0), 2)
+iva = round((total+descuento)*0.21, 2)
+
 print(
     f"{'Factura:':<15}",
     f"\n{'':<15}{l[3]:>10}",
@@ -20,7 +22,7 @@ print(
     f"\n{'Unidades':<15}{l[1]:>10}",
     f"\n{'Total':<15}{total:>10}",
     f"\n{'Descuento':<15}{descuento:>10}",
-    f"\n{'Total tras descuento':<15}{total-descuento:>10}",
-    f"\n{'I.V.A. (21%)':<15}{(total-descuento)*0.21:>10}",
-    f"\n{'Precio final':<15}{(total-descuento) + (total-descuento)*0.21:>10}",
+    f"\n{'Total tras descuento':<15}{total+descuento:>10}",
+    f"\n{'I.V.A. (21%)':<15}{iva:>10}",
+    f"\n{'Precio final':<15}{round((total+descuento) + iva, 2):>10}",
 )
