42e1590ebffa5b5ab93775f995aae265bfabbbf4
cardstdani <72665778+cardstdani@users.noreply.github.com>
2022-03-04 18:29:10 +0100

    Update Ejercicio10.py

--- Python/Ejercicio10.py
+++ Python/Ejercicio10.py
@@ -1,5 +1,5 @@
 while True:
-    inp = input("Introduce una secuencia de números enteros separados por un espacio:\n")
+    inp = input("Introduce una secuencia de números enteros separados por un espacio y que acabe en 0:\n")
     if inp[-1] != "0":
         print("La secuencia debe acabar en 0\n")
     else:
