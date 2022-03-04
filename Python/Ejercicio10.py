c571ab322b70ce8c4d52deb2054332e1e6b64394
cardstdani <72665778+cardstdani@users.noreply.github.com>
2022-03-04 18:28:17 +0100

    Create Ejercicio10.py

---
+++ Python/Ejercicio10.py
@@ -0,0 +1,15 @@
+while True:
+    inp = input("Introduce una secuencia de números enteros separados por un espacio:\n")
+    if inp[-1] != "0":
+        print("La secuencia debe acabar en 0\n")
+    else:
+        inp = [int(i) for i in inp[:-1].split()]
+        current = inp[0]
+        creciente = True
+        for i in inp:
+            if i >= current:
+                current = i
+            else:
+                creciente = False
+                break
+        print("\nCreciente" if creciente else "\nNo creciente")
